a86c5de115aad8e2af85f04da70bafdebd9b52e5
Dmitry Void <supsad@yahoo.com>
2022-04-22 02:14:12 +0700

    added autofocus for username popup function

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,13 @@
 const popupMessageUs = document.querySelector('.modal-message');
 const popupOpenButton = document.querySelector('.address__button');
 const popupCloseButton = popupMessageUs.querySelector('.modal-message__close-button');
+const popupUserName = popupMessageUs.querySelector('[name=popup-username]');
 
 popupOpenButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   popupMessageUs.classList.add('modal-message_show');
+
+  popupUserName.focus();
 });
 
 popupCloseButton.addEventListener('click', function (evt) {
