8ec8924e4558c3a3298fe96262452e9bb2208b5a
Dmitry <supsad@yahoo.com>
2022-04-22 23:14:20 +0700

    added working script for slider

--- js/index-slider.js
+++ js/index-slider.js
@@ -1,11 +1,36 @@
-const sliderSwitch = document.querySelectorAll('.slider__switch-button');
-const sliderBlocks = document.querySelectorAll('.slider');
-console.log(sliderSwitch);
-console.log(sliderBlocks);
+const switchElements = document.querySelectorAll('.slider__switch-button');
+const sliderElements = document.querySelectorAll('.slider');
 
-for (let i = 0; i < sliderSwitch.length; i++) {
-  sliderSwitch[i].addEventListener('click', function () {
-    console.log(sliderBlocks);
-    sliderBlocks[i].classList.add('slider_current');
+const currentItemChecker = function (current) {
+  let currentItem;
+  for (let i = 0; i < current.length; i++) {
+    if (current[i].classList.contains('slider_current') ||
+      current[i].classList.contains('slider__switch-button_current')) {
+      currentItem = current[i];
+    }
+  }
+
+  return currentItem;
+};
+
+const currentItemDel = function (currentButton, currentSlider) {
+  let removeActiveButton = currentItemChecker(currentButton);
+  removeActiveButton.classList.remove('slider__switch-button_current');
+  removeActiveButton.removeAttribute('disabled');
+
+  let removeActiveSlider = currentItemChecker(currentSlider);
+  removeActiveSlider.classList.remove('slider_current');
+};
+
+const addCurrentSliderHandler = function (currentButton, currentSlider) {
+  currentButton.addEventListener('click', function () {
+    currentItemDel(switchElements, sliderElements);
+    currentButton.classList.add('slider__switch-button_current');
+    currentButton.setAttribute('disabled', '');
+    currentSlider.classList.add('slider_current');
   });
+};
+
+for (let i = 0; i < sliderElements.length; i++) {
+  addCurrentSliderHandler(switchElements[i], sliderElements[i]);
 }
