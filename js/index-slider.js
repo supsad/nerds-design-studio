e47265571a4ccc890044aa3d93852e179fe2253c
Dmitry <supsad@yahoo.com>
2022-04-23 12:34:50 +0700

    refactor itemChecker and failure of the focus function

--- js/index-slider.js
+++ js/index-slider.js
@@ -1,12 +1,15 @@
 const switchElements = document.querySelectorAll('.slider__switch-button');
 const sliderElements = document.querySelectorAll('.slider');
 
-const currentItemChecker = function (current) {
+// let currentElementId;
+
+const currentItemChecker = function (items) {
   let currentItem;
-  for (let i = 0; i < current.length; i++) {
-    if (current[i].classList.contains('slider_current') ||
-      current[i].classList.contains('slider__switch-button_current')) {
-      currentItem = current[i];
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].classList.contains('slider_current') ||
+      items[i].classList.contains('slider__switch-button_current')) {
+      currentItem = items[i];
+      // currentElementId = i - 1;
     }
   }
 
@@ -22,12 +25,28 @@ const currentItemDel = function (currentButton, currentSlider) {
   removeActiveSlider.classList.remove('slider_current');
 };
 
+// This element don't work and I decided not to include it in the assembly
+
+// const addNextFocusHandler = function (items) {
+//   const firstItemId = 0;
+//   const lastItemId = switchElements.length - 1;
+//   currentItemChecker(items);
+//   if (currentElementId === lastItemId) {
+//     currentElementId = firstItemId;
+//     switchElements[currentElementId].focus();
+//   } else {
+//     currentElementId += 1;
+//     switchElements[currentElementId].focus();
+//   }
+// };
+
 const addCurrentSliderHandler = function (currentButton, currentSlider) {
   currentButton.addEventListener('click', function () {
     currentItemDel(switchElements, sliderElements);
     currentButton.classList.add('slider__switch-button_current');
     currentButton.setAttribute('disabled', '');
     currentSlider.classList.add('slider_current');
+    // addNextFocusHandler(switchElements);
   });
 };
 
